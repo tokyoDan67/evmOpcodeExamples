1d8d3370baa18477a726b7cb0330708249ce78f7
Daniel Yamagata <danielyamagata@Daniels-MacBook-Pro-7.local>
2022-08-25 00:40:24 -0400

    Got rid of setValue() in IVault

--- vaultExample/IVault.sol
+++ vaultExample/IVault.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 interface IVault {
-    function setValue(uint256) external;
     function getValue() external view returns (uint256);
 } // end of IVault
